89c626e102832c0a33b066482dbf5fd7ac5a60fe
nick.durbin <nick.durbin@gmail.com>
2019-10-30 20:33:25 -0400

    Added some tests to each suite.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from "react";
+import * as rtl from "react-testing-library";
+import Controls from './Controls';
+import "jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+test("checking buttons", async () => {
+  const spy = jest.fn()
+  expect(spy).not.toBeCalled();
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,38 @@
-// Test away
+import React from "react";
+import * as rtl from "react-testing-library";
+import Dashboard from './Dashboard';
+import "jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+test("<Dashboard /> snapshot", async () => {
+  const wrapper = rtl.render(<Dashboard />)
+  await wrapper.getByText(/locked/i);
+  expect(wrapper.asFragment()).toMatchSnapshot()
+})
+
+test("Check open and closed functionality", async () => {
+  const wrapper = rtl.render(<Dashboard />)
+  await wrapper.getByText(/open/i);
+
+  const closed = wrapper.getByText(/close gate/i);
+  
+  rtl.act(() => {
+    rtl.fireEvent.click(closed)
+  })
+
+  expect(wrapper.getByText(/open/i)).toHaveTextContent(/open gate/i)
+})
+
+test("Check locked and unlocked functionality", async () => {
+  const wrapper = rtl.render(<Dashboard />)
+  await wrapper.getByText(/unlocked/i);
+
+  const locked = wrapper.getByText(/locked/i);
+  
+  rtl.act(() => {
+    rtl.fireEvent.click(locked)
+  })
+
+  expect(wrapper.getByText(/unlocked/i)).toHaveTextContent(/locked/i)
+})
